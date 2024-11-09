f45225b87a6e62461a0db17802c79d1799263afd
pigeoncoder <113204216+Mamlesh18@users.noreply.github.com>
2024-11-09 22:09:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Rental from './legal_component/Rental_code/rental_react';
+import Sale from './legal_component/abortion/real-sale';
+import Adoption from './legal_component/real-adoption/real-ado-react';
+import Sales from './legal_component/sale_code/sale-react';
 import Mains from './legal_component/main/main_react';
 import Login from './legal_component/login/login_react';
 import Signup from './legal_component/signup/signup_react';
@@ -8,14 +11,22 @@ import Bot from './legal_component/chat-bot/chat-react';
 import DummyMains from './legal_component/main/dummy-main';
 
 function App() {
-  
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Mains />} />
+      <Route path="/" element={<DummyMains/>} />
+
+        <Route path="/main_react" element={<Mains />} />
         <Route path="/rental_react" element={<Rental />} />
+        <Route path="/sale-react" element={<Sales />} />
+        <Route path="/real-ado-react" element={<Adoption />} />
+
+        <Route path="/real-sale" element={<Sale />} />
+
         <Route path="/login_react" element={<Login />} />
         <Route path="/signup_react" element={<Signup />} />
+
         <Route path="/chat-react" element={<Bot />} />
       </Routes>
     </Router>
